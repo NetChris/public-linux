e0dec1844bf3b7b583eb4897ae6ccbe8375d63dd
Chris Simmons <chris.dev@netchris.com>
2019-04-08 18:18:20 -0700

    Comment

--- bash/snippets/get-chmod-octal.sh
+++ bash/snippets/get-chmod-octal.sh
@@ -1,4 +1,5 @@
 # This gives the chmod octal permissions for a file
+# From https://askubuntu.com/a/144925/7098
 #  $ ls -lac /etc/fstab
 #  -rw-r--r-- 1 root root 503 Apr  7 21:59 /etc/fstab
 #  $ stat -c %a /etc/fstab
