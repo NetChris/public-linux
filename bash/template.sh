1c649de2b52b3d86a37851875a3b4f8ef06d30d6
Chris Simmons <chris.dev@netchris.com>
2019-04-08 17:59:43 -0700

    Add bash template file

---
+++ bash/template.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# This is a template from which to copy new bash scripts
+# As the library grows, it can probably be removed as opcying and pasting
+# any bash script will do.
+
+echo "This is a template!"
