c1099eedd40a16f61c463f98c83be655056f4aa2
Chris Simmons <gitlab.com@netchris.com>
2019-04-22 18:37:31 +0000

    Add get-gitlab-keys.sh

---
+++ bash/scripts/get-gitlab-keys.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Places the keys for a given GitLab username
+# in ~/.ssh/authorized_keys
+
+if [ -z "$GITLAB_USERNAME" ]
+then
+      echo "\$GITLAB_USERNAME must be defined"
+      exit -1
+fi
+
+exit 0
+
+mkdir -p ~/.ssh
+
+if ! [[ -f ~/.ssh/authorized_keys ]]; then
+  echo "Creating new ~/.ssh/authorized_keys"
+  touch ~/.ssh/authorized_keys
+  chmod 644 ~/.ssh/authorized_keys
+fi
+
+curl https://gitlab.com/$GITLAB_USERNAME.keys -w "\n" >> ~/.ssh/authorized_keys
