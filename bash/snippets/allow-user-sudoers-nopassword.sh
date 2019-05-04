503eca83c84f713215d131a086e0bfd59af137a3
Chris Simmons <gitlab.com@netchris.com>
2019-05-04 17:31:20 +0000

    Delete allow-user-sudoers-nopassword.sh

--- bash/snippets/allow-user-sudoers-nopassword.sh
+++
@@ -1,4 +0,0 @@
-# Configure to allow $USER to sudo without password
-export SUDOERS_NO_PASSWORD=/etc/sudoers.d/050_$USER-nopasswd
-echo "$USER ALL=(ALL) NOPASSWD: ALL" | sudo tee $SUDOERS_NO_PASSWORD
-sudo chmod 0440 $SUDOERS_NO_PASSWORD
