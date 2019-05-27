9174adda3fad31f824e71d3fa4e8be3c9aacc33a
Chris Simmons <chris.dev@netchris.com>
2019-05-27 10:07:53 -0700

    Tweak

--- setups/docker/scripts/wipe-docker01.sh
+++ setups/docker/scripts/wipe-docker01.sh
@@ -17,12 +17,12 @@ die () {
 
 MOUNT_POINT=/netchris/fsmounts/docker01
 
-[ ! -f $MOUNT_POINT ] || die "$MOUNT_POINT does not exist in the filesystem"
+[ ! -f $MOUNT_POINT ] || die "\"$MOUNT_POINT\" does not exist in the filesystem"
 
 echo Getting LV path for mount point "$MOUNT_POINT" ...
 LV_PATH=`df | grep "$MOUNT_POINT" | awk '{print $1}'`
 
-[ -z "${LV_PATH+x}" ] || die "Could not find LV path for mount point $MOUNT_POINT"
+[ -z "${LV_PATH+x}" ] || die "Could not find LV path for mount point \"$MOUNT_POINT\""
 
 if mount | grep $MOUNT_POINT > /dev/null; then
   echo Unmounting $MOUNT_POINT
