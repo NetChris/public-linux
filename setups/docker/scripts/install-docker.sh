137d062cba2fbe1de5f41bea5664c2b5017c0d70
Chris Simmons <chris.dev@netchris.com>
2019-05-27 10:25:52 -0700

    install-docker.sh candidate

--- setups/docker/scripts/install-docker.sh
+++ setups/docker/scripts/install-docker.sh
@@ -18,7 +18,7 @@ MOUNT_POINT=/netchris/fsmounts/docker01
 
 [ ! -f $MOUNT_POINT ] || die "\"$MOUNT_POINT\" does not exist in the filesystem"
 
-exit
+echo Installing Docker ...
 
 # Install Docker
 # Distilled from https://docs.docker.com/install/linux/docker-ce/ubuntu/
@@ -46,14 +46,30 @@ apt-get install docker-ce docker-ce-cli containerd.io -y
 echo Shutting down Docker to configure ...
 service docker stop
 
-vi /etc/docker/daemon.json
-
-# Modify to general config - https://gitlab.com/NetChris/docker/reference/wikis/configuration/daemon-json
-# Remove /var/lib/docker
+echo Removing default Docker data directory "/var/lib/docker"
 rm -rf /var/lib/docker
-# Restart Docker
-sudo service docker start
-# Verify that new docker data root is being used
-sudo ls -lac /netchris/fsmounts/docker01/docker
-# Add chris to docker group
+
+echo Pulling NetChris baseline Docker daemon configuration to /etc/docker/daemon.json
+curl -sSL 'https://gitlab.com/NetChris/public/linux/raw/master/setups/docker/etc/docker/daemon.json' \
+  -o /etc/docker/daemon.json
+
+echo Making cert directories ...
+mkdir -p -m 755 /netchris/fsmounts/docker01/certs
+mkdir -p -m 750 /netchris/fsmounts/docker01/certs/keys
+
+echo Pulling down certificates ...
+curl -sSL https://gitlab.com/NetChris/public/key-certificates/raw/master/docker/ca-cert-15909179367545147508.pem \
+  -o /netchris/fsmounts/docker01/certs/ca-cert.pem
+curl -sSL https://gitlab.com/NetChris/public/key-certificates/raw/master/docker/wildcard.loc.network/server-cert.pem \
+  -o /netchris/fsmounts/docker01/certs/server-cert.pem
+
+echo Adding chris to docker group
 sudo usermod -a -G docker chris
+
+echo You will need to finish the rest:
+echo "  1. Copy/paste the Docker wildcard.loc.network server key to \"/netchris/fsmounts/docker01/certs/keys/server-key.pem\""
+echo "  2. Start Docker - sudo service docker start"
+echo "  3. Verify that Docker data directory is filled in: \"sudo ls -lac /netchris/fsmounts/docker01/docker\""
+echo "  4. Verify remote Docker connectivity.  From a remote machine:"
+echo "    docker --tlsverify --tlscacert=/path/to/ca-cert.pem --tlscert=/path/to/client-cert.pem \\"
+echo "      --tlskey=/path/to/client-key.pem -H=\"THIS_MACHINE.servers.dhcp.loc.network:2376\" info"
