0e824f272c81ed0ba66e035612cfbe6bc99a5394
Chris Simmons <chris.dev@netchris.com>
2019-07-15 19:19:38 -0700

    Syslog

--- bash/scripts/20190715-syslog-test.sh
+++ bash/scripts/20190715-syslog-test.sh
@@ -2,3 +2,8 @@
 # 2019-07-15 - Reinstall syslog configuration, test-swarm
 sudo rm -f /etc/rsyslog.d/60-*.conf
 sudo rm -f /etc/rsyslog.d/70-*.conf
+sudo curl -sSL \
+  https://gitlab.com/NetChris/public/linux/raw/master/Ubuntu/etc/rsyslog.d/70-syslog.test.loc.network.conf \
+  -o /etc/rsyslog.d/70-syslog.test.loc.network.conf
+sudo service rsyslog restart
+logger Testing 123 from $HOSTNAME
