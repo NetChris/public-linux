4094dc07b339f5a08ff58dd00c91990686b64c1d
Chris Simmons <chris.dev@netchris.com>
2019-07-15 19:28:23 -0700

    Production

---
+++ bash/scripts/20190715-syslog-production.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# 2019-07-15 - Reinstall syslog configuration, swarm
+# curl -sSL https://gitlab.com/NetChris/public/linux/raw/master/bash/scripts/20190715-syslog-production.sh | bash -s
+
+sudo rm -f /etc/rsyslog.d/60-*.conf
+sudo rm -f /etc/rsyslog.d/70-*.conf
+sudo curl -sSL \
+  https://gitlab.com/NetChris/public/linux/raw/master/Ubuntu/etc/rsyslog.d/70-syslog.loc.network.conf \
+  -o /etc/rsyslog.d/70-syslog.loc.network.conf
+sudo service rsyslog restart
+logger Testing 123 from $HOSTNAME
