85c6b7542e01b24d6624396f34a905f1a4496c39
Chris Simmons <chris.dev@netchris.com>
2019-08-22 18:48:50 -0700

    WIP

--- Ubuntu/baseline.sh
+++ Ubuntu/baseline.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 # Baseline Ubuntu script, post install
 # This script should be run after EVERY Ubuntu install
+# Before running this script, update and get the following packages:
+#  sudo apt update
+#  sudo apt upgrade
+#  sudo apt install jq
+
+if [[ -z "$GITLAB_PAT" ]]; then
+    echo "Must provide GITLAB_PAT (GitLab personal access token) in environment" 1>&2
+    exit 1
+fi
 
 # TODO - ensure no swap
+
+# We want curl to output endline after running
+echo '-w "\n"' >> ~/.curlrc
+
+# Generate an SSH key and send to GitLab
+SSH_KEY_TITLE="$USER@$HOSTNAME (`date +%s`)"
+ssh-keygen -t rsa -P "" -C "$SSH_KEY_TITLE" -f ~/.ssh/id_rsa
+SSH_KEY=`cat ~/.ssh/id_rsa.pub`
+JSON=`jq -c -n --arg title "$SSH_KEY_TITLE" --arg key "$SSH_KEY" '{title: $title, key: $key}'`
+echo $JSON | curl \
+  -X POST \
+  -i \
+  -H "Content-Type: application/json" \
+  -H "PRIVATE-TOKEN: ${GITLAB_PAT}" \
+  -d @- \
+  https://gitlab.com/api/v4/user/keys
