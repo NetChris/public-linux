38cad27f87647d486e6aa9db1b5108dd67084bf6
Chris Simmons <chris.dev@netchris.com>
2019-08-22 18:53:20 -0700

    WIP

--- Ubuntu/baseline.sh
+++ Ubuntu/baseline.sh
@@ -5,6 +5,8 @@
 #  sudo apt update
 #  sudo apt upgrade
 #  sudo apt install jq
+# TEMP
+# curl -X GET https://gitlab.com/cssl/NetChris/public/linux/raw/UbuntuBaseline/Ubuntu/baseline.sh | sudo bash -E
 
 if [[ -z "$GITLAB_PAT" ]]; then
     echo "Must provide GITLAB_PAT (GitLab personal access token) in environment" 1>&2
@@ -18,6 +20,7 @@ echo '-w "\n"' >> ~/.curlrc
 
 # Generate an SSH key and send to GitLab
 SSH_KEY_TITLE="$USER@$HOSTNAME (`date +%s`)"
+rm -f ~/.ssh/id_rsa
 ssh-keygen -t rsa -P "" -C "$SSH_KEY_TITLE" -f ~/.ssh/id_rsa
 SSH_KEY=`cat ~/.ssh/id_rsa.pub`
 JSON=`jq -c -n --arg title "$SSH_KEY_TITLE" --arg key "$SSH_KEY" '{title: $title, key: $key}'`
