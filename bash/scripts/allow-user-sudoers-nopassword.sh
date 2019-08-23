73119ea60e4c768d1519259f890ec7ffb9e2275a
Chris Simmons <gitlab.com@netchris.com>
2019-08-23 00:51:01 +0000

    allow-user-sudoers-nopassword - Just use USER for filename

--- bash/scripts/allow-user-sudoers-nopassword.sh
+++ bash/scripts/allow-user-sudoers-nopassword.sh
@@ -4,6 +4,6 @@
 # curl -sSL https://gitlab.com/NetChris/public/linux/raw/master/bash/scripts/allow-user-sudoers-nopassword.sh | bash -s
 
 # Configure to allow $USER to sudo without password
-export SUDOERS_NO_PASSWORD=/etc/sudoers.d/050_$USER-nopasswd
+export SUDOERS_NO_PASSWORD=/etc/sudoers.d/$USER
 echo "$USER ALL=(ALL) NOPASSWD: ALL" | sudo tee $SUDOERS_NO_PASSWORD
 sudo chmod 0440 $SUDOERS_NO_PASSWORD
