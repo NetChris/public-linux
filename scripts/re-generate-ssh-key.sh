2ea9a47c59333a12a85d1bfd27cf70c746ff3ed6
Chris Simmons <chris.dev@netchris.com>
2019-08-25 16:35:45 -0700

    Re-org, curl comment

--- scripts/re-generate-ssh-key.sh
+++ scripts/re-generate-ssh-key.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Re-generate the SSH key
+#   curl -sSL https://gitlab.com/cssl/NetChris/public/linux/raw/master/scripts/re-generate-ssh-key.sh | bash -s
 
 SSH_KEY_TITLE="$USER@$HOSTNAME (`date +%s`)"
 rm -f ~/.ssh/id_rsa ~/.ssh/id_rsa.pub
