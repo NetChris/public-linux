26aca986b4feafbb407a5abc085c3d9d20e24021
Chris Simmons <chris.dev@netchris.com>
2019-08-26 20:27:52 -0700

    Remove scripts now in cssl/ChrisSimmons/linux/home

--- scripts/allow-user-sudoers-nopassword.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Allows the current user to sudo without a password
-# Execute this with:
-# curl -sSL https://gitlab.com/NetChris/public/linux/raw/master/scripts/allow-user-sudoers-nopassword.sh | bash -s
-
-# Configure to allow $USER to sudo without password
-export SUDOERS_NO_PASSWORD=/etc/sudoers.d/$USER
-echo "$USER ALL=(ALL) NOPASSWD: ALL" | sudo tee $SUDOERS_NO_PASSWORD
-sudo chmod 0440 $SUDOERS_NO_PASSWORD
