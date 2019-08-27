9be3c3c05094e817f473413d794a1f65270481ca
Chris Simmons <chris.dev@netchris.com>
2019-08-26 20:34:44 -0700

    Revert "Remove script"
    This reverts commit b85a02171cf5437274540523b04906a1a03600f5.

---
+++ scripts/re-generate-ssh-key.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Re-generate the SSH key
+#   curl -sSL https://gitlab.com/cssl/NetChris/public/linux/raw/master/scripts/re-generate-ssh-key.sh | bash
+
+SSH_KEY_TITLE="$USER@$HOSTNAME (`date +%s`)"
+rm -f ~/.ssh/id_rsa ~/.ssh/id_rsa.pub
+ssh-keygen -t rsa -P "" -C "$SSH_KEY_TITLE" -f ~/.ssh/id_rsa
