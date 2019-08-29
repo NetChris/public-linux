f64a25bc52046d76eb09856baf7d56be6107f850
Chris Simmons <chris.dev@netchris.com>
2019-08-28 20:21:56 -0700

    Cut down script

--- Ubuntu/baseline.sh
+++ Ubuntu/baseline.sh
@@ -5,10 +5,8 @@
 #  sudo apt update
 #  sudo apt upgrade
 #  sudo apt install jq
-# TEMP - TODO:
-#   curl -X GET https://gitlab.com/cssl/NetChris/public/linux/raw/UbuntuBaseline/Ubuntu/baseline.sh | bash
-# TEMP cleanup:
-#   rm -rf ~/.ssh/ && rm -rf ~/src/ && rm -rf ~/.curlrc && rm -rf ~/.gitconfig
+# TEMP:
+#   curl -X GET https://gitlab.com/cssl/NetChris/public/linux/raw/master/Ubuntu/baseline.sh | bash
 
 if [[ -z "$GITLAB_PAT" ]]; then
     echo "Must provide GITLAB_PAT (GitLab personal access token) in environment" 1>&2
@@ -23,21 +21,3 @@ git clone git@gitlab.com:cssl/NetChris/Standards/linux.git \
 
 # This provides the real baseline setup for an Ubuntu system
 cat ~/src/standard-scripts/linux/Ubuntu/standard-setup.sh
-
-# TODO - Make this a script
-# # Split up ~/.bashrc into ~/.bashrc.d/
-# mkdir ~/.bashrc.d
-# chmod 700 ~/.bashrc.d
-# mv ~/.bashrc ~/.bashrc.d/original.bashrc
-
-# cat <<EOT >> ~/.bashrc
-# for file in ~/.bashrc.d/*.bashrc;
-# do
-# source "$file"
-# done
-# EOT
-# chmod --reference=".bashrc.d/.bashrc" ~/.bashrc
-
-# TODO - Place a "~/.refresh.sh" file that does a full refresh of the user's home directory with:
-# ~/.bashrc.d/ contents
-# ~/src/...
