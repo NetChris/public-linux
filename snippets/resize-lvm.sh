a6f2fb93c1c187e95017c52da99d8a07f6149dab
Chris Simmons <gitlab.com@netchris.com>
2019-09-22 18:05:11 +0000

    Update resize-lvm.sh

--- snippets/resize-lvm.sh
+++ snippets/resize-lvm.sh
@@ -1,2 +1,5 @@
-# Extend LVM "home" on VG "protectli-20190819-vg" to 10GB
+# Resize LVM "home" on VG "protectli-20190819-vg" to 10GB
 sudo lvresize -L 10G /dev/protectli-20190819-vg/home
+
+# Resize the filesystem (requires ext4)
+sudo resize2fs /dev/mapper/protectli--20190819--vg-home
